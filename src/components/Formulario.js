883f35bd00468ba3b6a9ccaec9ee88179555493d
Alejandro Isa <alejandroisa@gmail.com>
2021-02-09 23:29:39 -0300

    se agrega hook useCriptomoneda.js y el componente Error.js

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import styled from '@emotion/styled';
+import Error from './Error';
 
+
+//utilizar useMoneda
 import useMoneda from '../hooks/useMondeda';
+
+// Utilizar useCriptomoneda
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
+
 
 const Boton = styled.input`
     margin-top: 20px;
@@ -23,6 +31,10 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+    //state del listado de criptomonedas
+    const [listacripto, guardarCriptomonedas]= useState([]);
+    const [error, guardarError] = useState(false);
+
     const MONEDAS = [
         {codigo: 'USD', nombre:'Dolar de Estados Unidos'},
         {codigo: 'MXN', nombre:'Peso Mexicano'},
@@ -34,10 +46,43 @@ const Formulario = () => {
     // Utilizar useMoneda
     const [moneda, SelectMonedas] = useMoneda('Elige tu Moneda', '', MONEDAS);
 
+    // Utilizar useCriptomoneda
+    const [criptomoneda, SelectCripto]= useCriptomoneda('Elige tu Criptomoneda', '',listacripto);
+
+    //Ejecutar llamado a la API
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    },[]);
+
+    // Cuando el usuario hace submit
+    const cotizarMoneda = e =>{
+        e.preventDefault();
+
+        //validar si ambos campos estan llenos
+        if(moneda === '' || criptomoneda === ''){
+            guardarError(true);
+            return;
+        }
+
+        //pasar los datos al componente principal
+        guardarError(false);
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error ? <Error mensaje="Todos los campos son obligatorios"/> : null}
 
             <SelectMonedas/>
+
+            <SelectCripto/>
 
             <Boton
                 type="submit"
